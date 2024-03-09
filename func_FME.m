a909b063766fb4bae0633eff224b4eb1219d3c98
Simaocard_fixo <simaoppcardoso@gmail.com>
2024-03-09 15:33:37 +0000

    working and added documnetation

--- func_FME.m
+++ func_FME.m
@@ -1,25 +1,39 @@
 function vectFinal =  func_FME (vecdir,vecesq,vectcent,x,y,corrent,permnucleo,nespiras,argapesq,argpdir)
+% seting defaul/debuggin values en M
+vecdir = (3*30 + 3.5*6)*power(10,-3);
+vecesq = (3*30 + 3.5*6)*power(10,-3);
+vectcent = (30*1+ 3.5*2)*power(10,-3);
+x = 7*power(10,-3);
+y = 5*power(10,-3);
+corrent =1;
+permnucleo =2000;
+nespiras =300;
+argapesq = 0.07*power(10,-3); %m
+argpdir = 0.05*power(10,-3); %m
+
 #area contante
 Area = x*y;
 #retorna a reatancia
-Rdir = Reatancia(vecdir,Area,argpdir,permnucleo,0)
-Resq = Reatancia(vecesq,Area,argapesq,permnucleo,0)
-Rcent = Reatancia(vectcent,Area,0,permnucleo,1)
-Rarpgdir =Reatancia(vecdir,argpdir,0,1,5)
-Rarpgesq =Reatancia(vecesq,argapesq,0,1,5)
+Rdir = Reatancia(vecdir,Area,argpdir,permnucleo,0);
+Resq = Reatancia(vecesq,Area,argapesq,permnucleo,0);
+Rcent = Reatancia(vectcent,Area,0,permnucleo,1);
+%air gap
+Rarpgdir =Reatancia(argpdir,Area,0,1,5);
+Rarpgesq =Reatancia(argapesq,Area,0,1,5);
+%corrente e NI
 i= corrent;
-
 ni=nespiras*i;
-#debugin
 
 
-
+%calculo do total
 Rtotal = power((1/(Rdir+Rarpgdir)+1/(Resq+Rarpgesq)),-1)+Rcent
 
 fluxtotal = ni/Rtotal
-#fluxdir = (1/(Rarpgdir+Rdir)/((1/(Rarpgdir+Rdir))+(1/(Rarpgesq+Resq)))*fluxtotal
-#fluxdesqd = (1/(Rarpgesq+Resq)/((1/(Rarpgdir+Rdir))+(1/(Rarpgesq+Resq)))*fluxtotal
+% Calculando fluxo direito
+fluxdir = (1/(Rarpgdir+Rdir)/((1/(Rarpgdir+Rdir))+(1/(Rarpgesq+Resq))))*fluxtotal;
 
-
-#vectFinal = [fluxtotal,fluxdir,fluxdesqd]
+% Calculando fluxo esquerdo
+fluxdesqd = (1/(Rarpgesq+Resq)/((1/(Rarpgdir+Rdir))+(1/(Rarpgesq+Resq))))*fluxtotal;
+%resoltado finais en Ft=Fdir+Fesq
+vectFinal = [fluxtotal,fluxdir,fluxdesqd];
 endfunction
